d9599912d74991db3addac6e210ae033b6cb2ed3
Matthew Richardson <mat_richardson@mac.com>
2020-08-12 12:33:49 +1000

    Basic functionality

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -11,12 +11,35 @@ import SwiftUI
 struct ContentView: View {
         
     func calculateBedtime() {
+        let model = SleepCalculator()
         
+        let components = Calendar.current.dateComponents([.hour, .minute],from: wakeUp)
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minute = (components.minute ?? 0) * 60
+        
+        do {
+            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
+            
+            let sleepTime = wakeUp - prediction.actualSleep
+            
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            alertMesssage = formatter.string(from: sleepTime)
+            alertTitle = "Your ideal bedtime is.."
+            
+        } catch {
+            alertTitle = "Error"
+            alertMesssage = "Sorry, something went wrong"
+        }
+        showingAlert = true
     }
     
     @State private var wakeUp = Date()
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
+    @State private var alertTitle = ""
+    @State private var alertMesssage = ""
+    @State private var showingAlert = false
     
     var body: some View {
         NavigationView {
@@ -34,7 +57,7 @@ struct ContentView: View {
                     Text("\(sleepAmount, specifier: "%g") hours")
                 }
                 
-                Text("How much coffe?")
+                Text("How much coffee?")
                 
                 Stepper(value: $coffeeAmount, in: 1...20) {
                     if coffeeAmount == 1 {
@@ -51,6 +74,9 @@ struct ContentView: View {
                 Text("Calculate")
                 }
             )
+                .alert(isPresented: $showingAlert) {
+                    Alert(title: Text(alertTitle), message: Text(alertMesssage), dismissButton: .default(Text("OK")))
+            }
         }
     }
 }
